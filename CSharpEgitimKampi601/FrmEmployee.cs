69778e7bd1c4b69e37162c43b07983d176a4e20e
Emirhan Sartik <sartikemirhan@gmail.com>
2026-08-20 16:43:55 +0300

    Keep Working On PostgreSQL

---
+++ CSharpEgitimKampi601/FrmEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class FrmEmployee : Form
+    {
+        public FrmEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmEmployee_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+                
+        }
+    }
+}
